4c0235331d96b223fe65c3b749bc4af0c4f7381d
britrollo <britrollo@gmail.com>
2019-11-18 14:48:08 -0500

    still working on Trie

--- Trie.py
+++ Trie.py
@@ -14,6 +14,19 @@ class CompressedTrie:
     def _charToIndex(self, ch):
         return ord(ch)-ord('a')
 
+
+    def print(self, p):
+        ptr = p
+        if ptr.isLeaf:
+            return
+        while True:
+            print(ptr)
+            children = ptr.children
+            for child in children:
+                print(child.data)
+                self.print(child) 
+
+
     def insert(self, key):
         ptr = self.root
         length = len(key)
@@ -21,11 +34,20 @@ class CompressedTrie:
             letter = key[level]
             index = self._charToIndex(letter)
             if not ptr.children[index]:
-                ptr.children[index] = self.newNode(key[:level+1])
+                ptr.children[index] = self.newNode(key[level:])
+                ptr.children[index].isLeaf = True
+                return
             else:
                 node = ptr.children[index].data
+
                 ptr.children[index].data = node[0]
-                self.insert(node[1:])
+                ptr.isLeaf = False
+                # self._insert_below(ptr.children[index], node[1:])
+                cur = ptr.children[index]
+                index2 = self._charToIndex(node[1])
+                next = cur.children[index2]
+                next = self.newNode(node[1:])
+            
             ptr = ptr.children[index]
 
         ptr.isLeaf = True
@@ -39,3 +61,40 @@ class CompressedTrie:
                 return False
             ptr = ptr.children[index]
         return ptr != None and ptr.isLeaf
+
+    # driver function 
+def main(): 
+  
+    # Input keys (use only 'a' through 'z' and lower case) 
+    keys = ["the","a","there","anaswe","any", 
+            "by","their"] 
+    output = ["Not present in trie", 
+              "Present in trie"] 
+  
+    # Trie object 
+    t = CompressedTrie() 
+
+    t.insert("by")
+    t.insert("both")
+    
+    # # Construct trie 
+    # for key in keys: 
+    #     t.insert(key) 
+  
+    # # # Search for different keys 
+    # print("{} ---- {}".format("the",output[t.search("the")])) 
+    # print("{} ---- {}".format("these",output[t.search("these")])) 
+    # print("{} ---- {}".format("their",output[t.search("their")])) 
+    # print("{} ---- {}".format("thaw",output[t.search("thaw")])) 
+    for child in t.root.children:
+        if child:
+            print(child.data + " " + str(child.isLeaf))
+            for c in child.children:
+                if c:
+                    print(c.data + " " + str(c.isLeaf))
+   
+    # t.print(t.root)
+  
+if __name__ == '__main__': 
+    main() 
+  
