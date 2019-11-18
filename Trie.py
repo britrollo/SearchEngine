9e9a25f85b9bb47b9520a4bd946c8d39f650b44a
britrollo <britrollo@gmail.com>
2019-11-17 23:00:28 -0500

    Compressed Trie

--- Trie.py
+++ Trie.py
@@ -1,14 +1,15 @@
 class TrieNode:
-    def __init__(self):
+    def __init__(self,key):
         self.children=[None]*26
+        self.data=key
         self.isLeaf = False
 
-class Trie:
+class CompressedTrie:
     def __init__(self):
-        self.root = self.newNode()
+        self.root = self.newNode(None)
     
-    def newNode(self):
-        return TrieNode()
+    def newNode(self,key):
+        return TrieNode(key)
     
     def _charToIndex(self, ch):
         return ord(ch)-ord('a')
@@ -17,10 +18,14 @@ class Trie:
         ptr = self.root
         length = len(key)
         for level in range(length):
-            index = self._charToIndex(key[level])
-
+            letter = key[level]
+            index = self._charToIndex(letter)
             if not ptr.children[index]:
-                ptr.children[index] = self.newNode()
+                ptr.children[index] = self.newNode(key[:level+1])
+            else:
+                node = ptr.children[index].data
+                ptr.children[index].data = node[0]
+                self.insert(node[1:])
             ptr = ptr.children[index]
 
         ptr.isLeaf = True
