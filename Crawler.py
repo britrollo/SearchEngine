196f980c7213bcc9f16e85d951f77482242f38ff
britrollo <britrollo@gmail.com>
2019-11-30 00:52:35 -0500

    bug fixes, isWord for words that are contained in other words

--- Crawler.py
+++ Crawler.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-# from Trie import CompressedTrie
+from Trie import CompressedTrie
+from InvertedIndex import InvertedIndex
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+global cache
+global inv_idx
+cache = CompressedTrie()
+inv_idx = InvertedIndex()
 
 def page_load(url):
     """
@@ -8,28 +16,71 @@ def page_load(url):
     Parameters: string url - webpage url
     Return: string data - webpage HTML data
     """
-    r = requests.get("http://"+url)
-    data = r.text
+    r = requests.get(url)
+    data = r.content
     return data
 
 # TODO: Read data from webpage
-def page_read(webpage):
+def page_read(webpage, url):
     """
     Process data loaded from webpage
     Parameters: webpage - data loaded from webpage
     Return: None - all data is loaded into Trie
     """
-    with open(webpage) as wp:
-        soup = BeautifulSoup(wp)
+    soup = BeautifulSoup(webpage, 'html.parser')
 
-        # Get all hyperlinks
-        hyperlinks = soup.find_all('a')
+    # html = list(soup.children)[2]
+    # print(list(html.children))
 
-        #Get title
-        if len(list(soup.title)) == 1:
-            title = soup.title.string
+    # p tags 
+    p_tags = soup.find_all('p')
+    for p in p_tags:
+        txt = p.get_text()
+        txt = remove_punc(txt)
+        for w in txt:
+            w = w.lower()
+            if w not in set(stopwords.words('english')) and not w.isnumeric():
+                load_into_memory(w, url, 1)
 
-        #Get body
-        body = []
-        for st in soup.body.stripped_strings:
-            body += st
+    # title tags
+    title_tags = soup.find_all('title')
+    for title in title_tags:
+        txt = title.get_text()
+        txt = remove_punc(txt)
+        for w in txt:
+            w = w.lower()
+            if w not in set(stopwords.words('english')) and not w.isnumeric():
+                load_into_memory(w, url, 3)
+
+    # Get all hyperlinks - a tags
+    hyperlinks = soup.find_all('a', href=True)
+    for a in hyperlinks:
+        print(a['href'])
+
+def load_into_memory(w, url, c):
+    """
+    Adds word w to compressed trie, inverted index, and rank 
+    Parameters: string w    - word
+                string url  - url with owrd present
+                int c       - count for ranking
+    """
+    # Check existence in trie
+    if cache.search(w):
+        inv_idx.add_url(w, url, c)
+    else:
+        cache.insert(w)
+        inv_idx.add_word(w, url, c)
+        
+def remove_punc(txt):
+    tokenizer = RegexpTokenizer(r'\w+')
+    return tokenizer.tokenize(txt)
+
+
+if __name__ == '__main__': 
+    # print("in" in set(stopwords.words('english')))
+    url = "http://www.dataquest.io/blog/web-scraping-tutorial-python/"
+    webpage = page_load(url)
+    page_read(webpage, url)
+
+    # cache.print(cache.root, "", 0)
+    # print(cache.root)
