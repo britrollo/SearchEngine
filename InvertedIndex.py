8ce4bb5c9b5a3984302ce87202ed81ca744a0865
britrollo <britrollo@gmail.com>
2019-11-30 19:39:14 -0500

    bug fixes

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -2,11 +2,13 @@ class InvertedIndex:
     def __init__(self):
         self.idx = {}
         self.rank = {}
-
+# TODO : comment 
     def add_word(self, w, url, c):
         """
         Add new word,url pair to memory
         """
+        if url[-1] == "/":
+            url = url[:-1]
         self.idx[w] = [url]
         if url in self.rank.keys():
             self.rank[url][w] = c
@@ -14,14 +16,25 @@ class InvertedIndex:
             self.rank[url] = {}
             self.rank[url][w] = c
 
-
+# TODO : comment 
     def add_url(self, w, url, c):
         """
         Word already exists in memory, add url or update word rank for url
         """
+        if url[-1] == "/":
+            url = url[:-1]
         if url not in self.idx[w]:
-            self.add_word(w, url, c)
+            self.idx[w] = self.idx[w] + [url]
+            self.rank[url] = {}
+            self.rank[url][w] = c 
+        
+        if url in self.rank:
+            if w in self.rank[url]:
+                self.rank[url][w] = self.rank[url][w] + c
+            else:
+                self.rank[url][w] = c
         else:
-            self.rank[url][w] = self.rank[url][w] + c
+            self.rank[url] = {}
+            self.rank[url][w] = c
     
     
